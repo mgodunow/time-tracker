5ec53a3c1f0f023369fb168a9fd58b536a016d88
mgodunov <mikhailgodunow@gmail.com>
2024-07-03 13:57:31 +0300

    Add examples for swag

--- internal/models/models.go
+++ internal/models/models.go
@@ -3,30 +3,30 @@ package models
 import "time"
 
 type User struct {
-	ID             int    `json:"id"`
-	PassportNumber string `json:"passportNumber"`
-	Surname        string `json:"surname"`
-	Name           string `json:"name"`
-	Patronymic     string `json:"patronymic"`
-	Address        string `json:"address"`
+	ID             int    `json:"id" example:"1"`
+	PassportNumber string `json:"passportNumber" example:"AB1234567"`
+	Surname        string `json:"surname" example:"Smith"`
+	Name           string `json:"name" example:"John"`
+	Patronymic     string `json:"patronymic" example:"Michael"`
+	Address        string `json:"address" example:"123 Main St, City"`
 }
 
 type Workload struct {
-	TaskID int           `json:"taskId"`
-	Hours  int           `json:"hours"`
-	Minutes int          `json:"minutes"`
+	TaskID  int `json:"taskId" example:"1"`
+	Hours   int `json:"hours" example:"8"`
+	Minutes int `json:"minutes" example:"30"`
 }
 
 type Task struct {
-	ID        int       `json:"id"`
-	UserID    int       `json:"userId"`
-	StartTime time.Time `json:"startTime"`
-	EndTime   time.Time `json:"endTime"`
+	ID        int       `json:"id" example:"1"`
+	UserID    int       `json:"userId" example:"1"`
+	StartTime time.Time `json:"startTime" example:"2023-07-03T09:00:00Z"`
+	EndTime   time.Time `json:"endTime" example:"2023-07-03T17:00:00Z"`
 }
 
 type People struct {
-	Surname    string `json:"surname"`
-	Name       string `json:"name"`
-	Patronymic string `json:"patronymic"`
-	Address    string `json:"address"`
+	Surname    string `json:"surname" example:"Smith"`
+	Name       string `json:"name" example:"John"`
+	Patronymic string `json:"patronymic" example:"Michael"`
+	Address    string `json:"address" example:"123 Main St, City"`
 }
